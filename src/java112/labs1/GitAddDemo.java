25b770bf9ddfa2e73aa7b6e5dc5dfdd8c6707189
Eric Knapp <eknapp@madisoncollege.edu>
2018-01-24 15:52:24 -0600

    Adding new demo file to demo git add.

---
+++ src/java112/labs1/GitAddDemo.java
@@ -0,0 +1,26 @@
+package java112.labs1;
+
+/**
+ * @author Eric Knapp
+ * class GitAddDemo
+ */
+public class GitAddDemo {
+
+
+    /**
+     * Run the demo
+     */
+    public void run() {
+
+    }
+
+    /**
+     * The start of the program
+     * @param arguments The command line arguments.
+     */
+    public static void main(String[] arguments) {
+        GitAddDemo demo = new GitAddDemo();
+        demo.run();
+    }
+
+}
