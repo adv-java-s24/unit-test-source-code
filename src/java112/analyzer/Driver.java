2b61549a2f54fd2fb0c658e063d7e196bfc4176c
Eric Knapp <eknapp@madisoncollege.edu>
2018-02-01 20:03:52 -0600

    Main analyze method coded and running.

--- src/java112/analyzer/Driver.java
+++ src/java112/analyzer/Driver.java
@@ -1,11 +1,21 @@
 package java112.analyzer;
 
+
 /**
+ *
+ *
  * @author Eric Knapp
  * class Driver
  */
 public class Driver {
 
-
-
+    /**
+     *
+     * @param arguments The command line arguments.
+     */
+    public static void main(String[] arguments) {
+        FileAnalysis analyzer = new FileAnalysis();
+        analyzer.analyze(arguments);
+    }
 }
+
--- src/java112/analyzer/FileAnalysis.java
+++ src/java112/analyzer/FileAnalysis.java
@@ -1,15 +1,21 @@
 package java112.analyzer;
 
+
 /**
  * @author Eric Knapp
  * class FileAnalysis
  */
 public class FileAnalysis {
+
     /**
      * Constructor for FileAnalysis
      */
     public FileAnalysis() {
-
     }
 
+
+    public void analyze(String[] arguments) {
+        System.out.println("in FileAnalysis.analyze()");
+    }
 }
+
