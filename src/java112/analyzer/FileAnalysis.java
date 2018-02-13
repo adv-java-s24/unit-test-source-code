871ba14ca59461d68c7792eede6e026fc0593b96
Eric Knapp <eknapp@madisoncollege.edu>
2018-02-13 13:53:04 -0600

    Refactoring a method name for clarity and precision.

--- src/java112/analyzer/FileAnalysis.java
+++ src/java112/analyzer/FileAnalysis.java
@@ -40,7 +40,7 @@ public class FileAnalysis {
         String inputFilePath = arguments[0];
 
         createFileAnalyzers();
-        analyzeInputFile(inputFilePath);
+        openInputFile(inputFilePath);
         writeOutputFiles(inputFilePath);
 
         long end = System.currentTimeMillis();
@@ -58,7 +58,7 @@ public class FileAnalysis {
     /**
      *
      */
-    private void analyzeInputFile(String inputFilePath) {
+    private void openInputFile(String inputFilePath) {
         //System.out.println("inputFilePath: " + inputFilePath);
 
         try (BufferedReader fileReader = new BufferedReader(new FileReader(inputFilePath))) {
