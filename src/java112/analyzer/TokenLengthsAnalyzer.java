17b9e0c17c5434f7e604142de819a91c1dc4ee97
Eric Knapp <eknapp@madisoncollege.edu>
2018-03-18 15:19:55 -0500

    Working on the the token lengths analyzer. It is counting the lengths and outputing the list of values.

--- src/java112/analyzer/TokenLengthsAnalyzer.java
+++ src/java112/analyzer/TokenLengthsAnalyzer.java
@@ -9,7 +9,7 @@ import java112.utilities.*;
  * @author Eric Knapp
  * class TokenLengthsAnalyzer
  */
-public class TokenLengthsAnalyzer {
+public class TokenLengthsAnalyzer implements TokenAnalyzer {
 
     private Map<Integer, Counter> tokenLengths;
     private Properties properties;
@@ -27,4 +27,50 @@ public class TokenLengthsAnalyzer {
         this.properties = properties;
     }
 
+    public Map<Integer, Counter> getTokenLengths() {
+        return tokenLengths;
+    }
+
+    public void processToken(String token) {
+
+        Integer tokenLength = token.length();
+
+        if (tokenLengths.containsKey(tokenLength)) {
+            tokenLengths.get(tokenLength).increment();
+        } else {
+            tokenLengths.put(tokenLength, new Counter());
+        }
+    }
+
+    public void generateOutputFile(String inputFilePath) {
+
+        String oututFilePath = properties.getProperty("output.directory") + properties.getProperty("output.file.token.lengths");
+
+        try (PrintWriter output = new PrintWriter(new BufferedWriter(new FileWriter(oututFilePath)))) {
+
+            writeTokenLengthsCounts(output);
+
+        } catch (IOException inputOutputException) {
+            inputOutputException.printStackTrace();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    private void writeTokenLengthsCounts(PrintWriter output) {
+        for (Map.Entry<Integer, Counter> entry : tokenLengths.entrySet()) {
+            output.println(entry.getKey() + "\t" + entry.getValue());
+        }
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
